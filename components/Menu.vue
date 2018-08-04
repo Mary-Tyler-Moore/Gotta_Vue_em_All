b870a8b68db53a7f571e224134948aeb676b2c6b
Kin Chan <xkinchanx@gmail.com>
2018-08-03 22:21:43 -0700

    [feat] nuxt routing

---
+++ components/Menu.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="pokemonMenu">
+    <nuxt-link class="link" to="/">
+      Home
+    </nuxt-link>
+    <nuxt-link class="link" to="/wild">
+      Wild
+    </nuxt-link>
+  </div>
+</template>
+
+<style scoped>
+#pokemonMenu {
+  background-color: green;
+  height: 5vh;
+  width: 100%;
+}
+
+.link {
+  color: white;
+  font-weight: bold;
+  justify-content: center;
+  padding: 5px;
+  text-decoration: none;
+}
+
+#pokemonMenu,
+.link {
+  align-items: center;
+  display: flex;
+}
+
+</style>
