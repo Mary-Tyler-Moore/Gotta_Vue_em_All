1d3c8f9777296fb0fa18c4f4699e7c4d935f02ae
Kin Chan <xkinchanx@gmail.com>
2018-08-04 13:56:16 -0700

    [prog] wild page is displaying random pokemon on load.  Moving on to catching feature

--- pages/wild.vue
+++ pages/wild.vue
@@ -1,16 +1,38 @@
 <template>
-  <div>
-    {{ text }}
+  <div id="wild" class="flexCenter page flexColumn">
+    <PokemonDisplay 
+      :img="this.$store.state.pokemon[index].img"/>
+      <div>{{ this.$store.state.pokemon[index].name }}</div>
   </div>
 </template>
 
 <script>
+  import { store } from '../store/store.js';
+  import PokemonDisplay from '../components/PokemonDisplay.vue';
+
   export default {
     name: 'wild',
+    components: {
+      PokemonDisplay,
+    },
+    store,
     data() {
       return {
-        text: 'Hello from the wild'
+
+      }
+    },
+    computed: {
+      index(){
+        return Math.floor(Math.random() * this.$store.state.pokemon.length);
       }
     }
   }
 </script>
+
+<style scoped>
+  #wild {
+    background-image: url("https://orig00.deviantart.net/6661/f/2014/325/d/d/or_as_battle_background_1_by_phoenixoflight92-d83m36e.png");
+    background-repeat: no-repeat;
+    background-size: cover;
+  }
+</style>
