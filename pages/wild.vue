edd105a231b403a749fce9dd0ab14daa4f3a6bbd
Kin Chan <xkinchanx@gmail.com>
2018-08-04 14:48:36 -0700

    [prog] swap between pokemon and ball onclick with transition

--- pages/wild.vue
+++ pages/wild.vue
@@ -1,8 +1,14 @@
 <template>
   <div id="wild" class="flexCenter page flexColumn">
-    <PokemonDisplay 
-      :img="this.$store.state.pokemon[index].img"/>
-      <div>{{ this.$store.state.pokemon[index].name }}</div>
+    <transition name="pokemonDisplay" mode="out-in">
+      <div key="1" v-if="!swapDisplay" @click="attemptCatch" class="flexCenter flexColumn">
+        <PokemonDisplay 
+        :img="this.$store.state.pokemon[index].img"/>
+        <div>{{ this.$store.state.pokemon[index].name }}</div>
+      </div>
+      <div key="2" v-else class="pokeball" @click="toggle">
+      </div>
+    </transition>
   </div>
 </template>
 
@@ -18,12 +24,24 @@
     store,
     data() {
       return {
-
+        index: Math.floor(Math.random() * this.$store.state.pokemon.length),
+        swapDisplay: false,
       }
     },
-    computed: {
-      index(){
-        return Math.floor(Math.random() * this.$store.state.pokemon.length);
+    methods: {
+      attemptCatch() {
+        let chance = Math.random() * 1;
+        if (chance > 0.60) {
+          console.log('success');
+        } else {
+          console.log('failed');
+        }
+        
+        this.index = Math.floor(Math.random() * this.$store.state.pokemon.length);
+        this.toggle();
+      },
+      toggle() {
+        this.swapDisplay = !this.swapDisplay;
       }
     }
   }
@@ -34,5 +52,21 @@
     background-image: url("https://orig00.deviantart.net/6661/f/2014/325/d/d/or_as_battle_background_1_by_phoenixoflight92-d83m36e.png");
     background-repeat: no-repeat;
     background-size: cover;
+  }
+
+  .pokemonDisplay-enter-active, .pokemonDisplay-leave-active {
+    transition: transform 1s;
+  }
+
+  .pokemonDisplay-enter, .pokemonDisplay-leave-to {
+    transform: scale(0.1);
+  }
+
+  .pokeball {
+    background-color: white;
+    border: 1px solid black;
+    border-radius: 100%;
+    height: 100px;
+    width: 100px;
   }
 </style>
