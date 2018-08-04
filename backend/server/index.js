0cb67c42c68c74104e1eafbc41e72c7de28763b2
Kin Chan <xkinchanx@gmail.com>
2018-08-04 13:53:43 -0700

    [fix] added cors to allow CO

--- backend/server/index.js
+++ backend/server/index.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import parser from 'body-parser';
 import router from './router';
+import cors from 'cors';
 import db from '../database/index';
 
 const app = express();
 const port = 3001;
 
+app.use(cors())
 app.use(parser.json());
 app.use(parser.urlencoded({ extended: true }));
 app.use('/api', router);
