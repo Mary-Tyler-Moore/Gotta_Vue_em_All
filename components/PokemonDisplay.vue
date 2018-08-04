db7e73cd3346a1601051c18f22f7de4645ccb55b
Kin Chan <xkinchanx@gmail.com>
2018-08-04 13:51:34 -0700

    [feat] reusable pokemon display component

---
+++ components/PokemonDisplay.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="pokemonDisplay">
+    <img :src="img" alt="pokemon image"/>
+    <slot />
+  </div>
+</template>
+
+<script>
+  export default {
+    props: ["img", "name"]
+  }
+</script>
+
+<style scoped>
+  #pokemonDisplay {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+  }
+</style>
