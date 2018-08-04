65d6ca2a236167ffffe69ce92dc225240a9a1cdb
Kin Chan <xkinchanx@gmail.com>
2018-08-03 22:21:22 -0700

    [feat] different pages for app

---
+++ pages/wild.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    {{ text }}
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'wild',
+    data() {
+      return {
+        text: 'Hello from the wild'
+      }
+    }
+  }
+</script>
