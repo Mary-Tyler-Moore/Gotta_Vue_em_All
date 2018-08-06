f3713f6c2d3931da46a9e5725ae3e94360973413
Kin Chan <xkinchanx@gmail.com>
2018-08-05 22:38:57 -0700

    [feat] filter component, pending functionality

---
+++ components/Filter.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="filterContainer">
+    <select>
+      <option selected disabled>type</option>
+      <option v-for="type in filterTypes" :key="type" :value="type">{{ type }}</option>
+    </select>
+    <select>
+      <option selected disabled>order</option>
+      <option value="desc">decending</option>
+      <option value="asce">ascending</option>
+    </select>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: ["filterTypes"]
+  }
+</script>
+
+<style scoped>
+  #filterContainer {
+    background-color: white;
+    height: 5vh;
+    width: 80%;
+    margin-top: 10px;
+  }
+</style>
