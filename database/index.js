f109359d2d2d126c90de82770f2513915d99bbb7
Kin Chan <xkinchanx@gmail.com>
2018-08-08 15:36:21 -0700

    [deploy] settings for heroku

--- database/index.js
+++ database/index.js
@@ -1,5 +1,7 @@
 import mongoose from 'mongoose';
-mongoose.connect('mongodb://localhost/gottavueemall');
+const database = process.env.DATABASE || 'mongodb://localhost/gottavueemall';
+
+mongoose.connect(database);
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error'));
